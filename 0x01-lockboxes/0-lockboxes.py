bfe8a87c106389f1a7df7293d349b00158be0454
Naasor <amofasandra7@gmail.com>
2024-06-06 04:30:20 +0000

    LockBoxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,20 +6,12 @@ sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
 
 
 def canUnlockAll(boxes):
-    """ Write a method that determines if all the boxes can be opened.
+    """ Method that determines if all the boxes can be opened.
     @boxes is a list of lists
     """
-    n = len(boxes)
-    unlocked = set([0])  # Start with the first box unlocked
-    keys = list(boxes[0])  # Start with the keys in the first box
-    queue = keys[:]
-    
-    while queue:
-        key = queue.pop(0)
-        if key < n and key not in unlocked:
-            unlocked.add(key)
-            for new_key in boxes[key]:
-                if new_key not in unlocked:
-                    queue.append(new_key)
-    
-    return len(unlocked) == n
+    keys = [0]
+    for n in keys:
+        for key in boxes[n]:
+            if key not in keys and key < len(boxes):
+                keys.append(key)
+    return len(keys) == len(boxes)
