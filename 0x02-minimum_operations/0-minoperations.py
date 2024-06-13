34cce13a2fe93b7f2d759321f098a9bd571a670c
Naasor <amofasandra7@gmail.com>
2024-06-13 23:17:32 +0000

    minimum operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Minimum Operations python3 challenge'''
+
+
+def minOperations(n):
+    '''calculates the fewest number of
+    operations needed to result in exactly n H
+    characters in this file.
+    Returns:
+        Integer : if n is impossible to achieve, return 0
+    '''
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
