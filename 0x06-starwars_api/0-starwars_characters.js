974de075885001862b51010daaec3380a3e61d00
Naasor <amofasandra7@gmail.com>
2024-07-11 12:21:04 +0000

    Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,34 @@
-import requests
-import sys
+#!/usr/bin/node
+/**
+  * prints all characters of a Star Wars movie
+  */
 
-def get_film_characters(movie_id):
-    # Base URL for the Star Wars API
-    base_url = 'https://swapi.dev/api/films/'
+const args = process.argv;
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${args[2]}`;
 
-    try:
-        # Get the film details using the movie ID
-        response = requests.get(f"{base_url}{movie_id}/")
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print(f"HTTP error occurred: {err}")
-        return
-    except Exception as err:
-        print(f"An error occurred: {err}")
-        return
+request.get(url, async (error, response, body) => {
+  if (error || response.statusCode !== 200) {
+    console.error('Error:', error || `Status code: ${response.statusCode}`);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (let i = 0; i < characters.length; i++) {
+      await getCharacterName(characters[i]);
+    }
+  }
+});
 
-    film_data = response.json()
-
-    # Get the list of character URLs
-    character_urls = film_data.get('characters', [])
-
-    # Print each character name
-    for url in character_urls:
-        character_response = requests.get(url)
-        character_data = character_response.json()
-        print(character_data.get('name', 'Unknown'))
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <movie_id>")
-    else:
-        movie_id = sys.argv[1]
-        get_film_characters(movie_id)
-
+function getCharacterName (character) {
+  return new Promise((resolve, reject) => {
+    request.get(character, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        console.error('Error:', error || `Status code: ${response.statusCode}`);
+        reject(error);
+      } else {
+        const characterInfo = JSON.parse(body);
+        console.log(characterInfo.name);
+        resolve(true);
+      }
+    });
+  });
+}
