0477f4751e3094e421133c010ca7e27c51f73f96
Naasor <amofasandra7@gmail.com>
2024-07-25 23:12:55 +0000

    fixing pycodestyle error

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,13 +7,14 @@ Module for making change
 def makeChange(coins, total):
     """
     Determine the fewest number of coins needed to meet a given amount total.
-    
+
     Args:
     coins (list): A list of the values of the coins in your possession.
     total (int): The total amount of money to make change for.
-    
+
     Returns:
-    int: The fewest number of coins needed to meet the total, or -1 if the total cannot be met by any number of coins.
+    int: The fewest number of coins needed to meet the total, or 
+    -1 if the total cannot be met by any number of coins.
     """
     if total <= 0:
         return 0
